856ff39a4ab3e31f665487928f075ef5ccf757a9
Andy Sipe <ajs.general@gmail.com>
2014-02-17 14:56:19 -0500

    added it project and cleaned up assemblyinfos

---
+++ src/DeleteStuff.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeleteStuff.IntegrationTests")]
+[assembly: AssemblyDescription("Delete Stuff Integration Tests")]
+[assembly: Guid("60e8a720-9626-429e-b5ee-033207da5379")]
