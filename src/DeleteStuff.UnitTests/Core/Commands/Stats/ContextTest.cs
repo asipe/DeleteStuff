cc8894f76def27832c1c9b25dbb19a0b1a2d05fe
Andy Sipe <ajs.general@gmail.com>
2014-06-03 07:58:49 -0400

    removed extra array setup

--- src/DeleteStuff.UnitTests/Core/Commands/Stats/ContextTest.cs
+++ src/DeleteStuff.UnitTests/Core/Commands/Stats/ContextTest.cs
@@ -6,7 +6,7 @@ namespace DeleteStuff.UnitTests.Core.Commands.Stats {
   public class ContextTest : BaseTestCase {
     [Test]
     public void TestDefaults() {
-      var context = new Context(BA("a", "b", "c"));
+      var context = new Context("a", "b", "c");
       Assert.That(context.Args, Is.EqualTo(BA("a", "b", "c")));
     }
   }
